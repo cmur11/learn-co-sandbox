0923ca1935f9384d674d622043651dfe0e35ff5d
Conor Murnane <conormurnane11@gmail.com>
2020-11-18 02:33:44 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,7 @@
+puts "Hello World"
+
+def multiply(num1, num2)
+  puts "multiply got called with #{num1} and #{num2}"
+  num1 * num2
+end
+multiply(2,4)
---
+++ greeting.rb
@@ -0,0 +1,35 @@
+# def greeting
+#     puts "Hello World!"
+# end
+
+# def say_greeting_five_times
+#     greeting
+#     greeting
+#     greeting
+#     greeting
+#     greeting
+# end
+
+# say_greeting_five_times
+
+# def greeting_a_person(name ='Conor',age)
+#     puts "Hello #{name}!, happy #{age}th birthday!"
+# end
+
+# greeting_a_person(,29)
+
+# def greeting_a_person(name,language ="ruby")
+#     puts "Hello #{name}! I hear you are a great #{language} programmer"
+# end
+
+# greeting_a_person("Conor")
+
+def a_method(a,b)
+  puts "I got #{a}"
+  puts "I got #{b}"
+  sum = a + b
+  puts "I got #{sum}"
+  sum
+end
+ 
+a_method(2,3)
